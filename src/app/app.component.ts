9046c5639a50f205d9848943f9debfd5cca21650
arunshankar22 <arun.zorro@gmail.com>
2018-04-09 15:51:31 +0800

    add proc file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { users } from './app.users';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public data: any[] = users;
 }
---
+++ src/app/search/app.users.ts
@@ -0,0 +1,49 @@
+export const users = [{
+  'id': 1,
+  'name': 'Suzi Cuningham',
+  'email': 'suzi.cuningham@gmail.com'
+}, {
+  'id': 2,
+  'name': 'Bobby Daniels',
+  'email': 'bobby.d@outlook.com'
+}, {
+  'id': 3,
+  'name': 'John Walker',
+  'email': 'johnywalker@blue.com'
+}, {
+  'id': 4,
+  'name': 'Eddy Stevens',
+  'email': 'estevens@yahoo.com'
+}, {
+  'id': 5,
+  'name': 'Jan Williams',
+  'email': 'jdubs@msn.com'
+}, {
+  'id': 6,
+  'name': 'Arun Shankar',
+  'email': 'arun.zorro@gmail.com'
+}, {
+  'id': 7,
+  'name': 'John Cena',
+  'email': 'john.cena@fb.com'
+}, {
+  'id': 8,
+  'name': 'Rock',
+  'email': 'therock@outlook.com'
+}, {
+  'id': 9,
+  'name': 'TripleH',
+  'email': 'th@msn.com'
+}, {
+  'id': 10,
+  'name': 'Shawn Michels',
+  'email': 'smichels@yahoo.com'
+}, {
+  'id': 11,
+  'name': 'Kane',
+  'email': 'kane@green.com'
+}, {
+  'id': 12,
+  'name': 'Hulkhogan',
+  'email': 'hulkh@wwf.com'
+}];
